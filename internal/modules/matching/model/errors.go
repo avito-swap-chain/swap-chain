1eece24dc9926568f6c0017cf250944eafbbcf01
nikgus <ngusev@gilmon.pro>
2026-08-08 13:31:01 +0500

    chore: move error declarations to a single block

--- internal/modules/matching/model/errors.go
+++ internal/modules/matching/model/errors.go
@@ -2,7 +2,9 @@ package model
 
 import "errors"
 
-var ErrVertexAlreadyExists = errors.New("vertex already exists")
-var ErrVertexNotFound = errors.New("vertex not found")
-var ErrEdgeAlreadyExists = errors.New("edge already exists")
-var ErrEdgeNotFound = errors.New("edge not found")
+var (
+	ErrVertexAlreadyExists = errors.New("vertex already exists")
+	ErrVertexNotFound      = errors.New("vertex not found")
+	ErrEdgeAlreadyExists   = errors.New("edge already exists")
+	ErrEdgeNotFound        = errors.New("edge not found")
+)
