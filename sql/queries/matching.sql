b953202e4efa6351ea8f7210a2ca2706c9f983d7
nikgus <ngusev@gilmon.pro>
2026-08-08 13:53:04 +0500

    chore(db): rename function

--- sql/queries/matching.sql
+++ sql/queries/matching.sql
@@ -1,7 +1,7 @@
--- name: GetTopK :many
+-- name: FindSimilarItems :many
 SELECT i1.id, i1.user_id, i1.offer_title, i1.offer_description, i1.want_description,
-       i1.offer_embedding::vector AS offer_embedding, 
-       i1.want_embedding::vector AS want_embedding, 
+       i1.offer_embedding::vector AS offer_embedding,
+       i1.want_embedding::vector AS want_embedding,
        (1.0 - (i1.offer_embedding <=> (SELECT i2.want_embedding FROM items i2 WHERE i2.id = $1)))::float8 AS similarity
 FROM items i1
 WHERE i1.id != $1
